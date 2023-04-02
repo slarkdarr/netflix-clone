71d862c6dc30b0c164f9ba69939f8ba63b299fa4
daffaresyaly <daffaresyaly@gmail.com>
2023-04-02 22:24:46 +0700

    fixed username

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { signOut } from 'next-auth/react';
 
+import useCurrentUser from '@/hooks/useCurrentUser';
+
 interface AccountMenuProps {
   visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data } = useCurrentUser();
+
   if (!visible) return null;
 
   return (
@@ -18,7 +22,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
             alt=""
           />
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
